b7d106ad252ba992b5c7a7f4128f82e18d6e9302
giorgia220523 <cocisgiorgia@gmail.com>
2020-10-07 19:33:59 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyAppGiorgia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
